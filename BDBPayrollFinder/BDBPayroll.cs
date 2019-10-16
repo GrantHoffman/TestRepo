60f2da803743cfd2a118850db34acb8236988880
Grant Hoffman <gHoffman@Viventium.com>
2019-10-16 17:11:59 -0400

    initial commit

---
+++ BDBPayrollFinder/BDBPayroll.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BDBPayrollFinder
+{
+    /// <summary>
+    /// This class is a  test to see if it's found by the crawler
+    /// </summary>
+    public class BDBPayroll
+    {
+        public string BDB = "bdbpayroll";
+    }
+}
